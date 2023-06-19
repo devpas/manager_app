6a81a8130043e67109b02e23c01f1d094f21f41c
devpas <dev@pas.vn>
2023-06-19 07:47:00 +0700

    app demo ver 1.00

--- windows/lib/modify/repository/base_repository.dart
+++ windows/lib/modify/repository/base_repository.dart
@@ -5,4 +5,9 @@ abstract class BaseRepository {
   Future<ApiResult<BaseResponse>> getListBase();
   Future<dynamic> checkDataFolder();
   Future<dynamic> createDataFolder();
+  Future<dynamic> addEmployee(dynamic dataEmployee);
+  Future<dynamic> updateEmployee(dynamic dataEmployee);
+  Future<dynamic> deleteEmployee(String email);
+  Future<dynamic> checkAccessBlock();
+  Future<ApiResult<EmployeeResponse>> getListEmplyees();
 }
